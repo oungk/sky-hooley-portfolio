921a3474cbd4d877c03411923f19ba3611bbe4c3
Katherine Oung <katherine.oung@gmail.com>
2026-07-14 19:27:55 -0500

    domain paths

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/sky-hooley-portfolio/', // required for GitHub Pages subpath
+  base: '/', // required for GitHub Pages subpath
 })
